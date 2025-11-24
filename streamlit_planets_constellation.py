5428ae41a388bbe8311e6e9318632e89c9238f03
Subramanian Ramajayam <maniramramajayam@gmail.com>
2025-11-24 22:54:24 +0530

    Add files via upload

--- streamlit_planets_constellation.py
+++ streamlit_planets_constellation.py
@@ -2,18 +2,6 @@ import streamlit as st
 import ephem
 import pandas as pd
 import datetime
-st.markdown(
-    """
-    <style>
-    body, .stApp {
-        background: linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%,#fad0c4 100%);
-        min-height: 100vh;
-        background-attachment: fixed;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # 🎨 Styling
 st.set_page_config(page_title="Planet Ephemeris Viewer", layout="centered")
