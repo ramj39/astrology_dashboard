723a50556bd616395a4af74395136552ee49620d
Subramanian Ramajayam <maniramramajayam@gmail.com>
2025-11-24 23:08:56 +0530

    Add files via upload

--- streamlit_planets_constellation.py
+++ streamlit_planets_constellation.py
@@ -27,7 +27,7 @@ planet_names = {
 }
 
 st.title("📆 Planet Ephemeris Viewer")
-
+st.markdown("thanks for using the app developed by subramanian ramajayam")
 # 🌍 Select planet
 selected_planet = st.selectbox("Choose a planet", list(planet_names.keys()))
 
@@ -76,4 +76,4 @@ else:
 
     # 📤 Optional CSV export
     st.download_button("Download CSV", df.to_csv(index=False), file_name=f"{selected_planet}_ephemeris.csv")
-st.markdown("Crafted with resonance by SubramanianRamajayam. Powered by the stars. Guided by data. 📿🖤")
+st.markdown("Crafted with resonance by SubramanianRamajayam. Powered by the stars. Guided by data. 📿")
